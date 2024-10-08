c674a55d34314d9ac79a34b01e1b621404b9a6d0
Musa Liman <musaliman27@gmail.com>
2024-10-08 11:00:42 +0100

    lightbox fix

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
 import Slider from 'react-slick';
-import Lightbox from 'react-image-lightbox';
 import {
   FacebookShareButton,
   TwitterShareButton,
   PinterestShareButton
 } from 'react-share';
-import 'react-image-lightbox/style.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
@@ -50,13 +48,11 @@ const Gallery = () => {
     <div className="gallery-container bg-white py-4 mb-20">
       {/* Section Heading */}
       <h1 className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl font-headline text-center my-6 sm:my-8 text-gray-900">
-  Experience Neon Backdrops in Action
-  <span className="block text-base sm:text-lg lg:text-xl text-gray-600 mt-3 sm:mt-4">
-    Explore how our vibrant backdrops transform real-life events into unforgettable moments.
-  </span>
-</h1>
-
-
+        Experience Neon Backdrops in Action
+        <span className="block text-base sm:text-lg lg:text-xl text-gray-600 mt-3 sm:mt-4">
+          Explore how our vibrant backdrops transform real-life events into unforgettable moments.
+        </span>
+      </h1>
 
       {/* Slider/Carousel */}
       <Slider {...settings}>
@@ -75,35 +71,37 @@ const Gallery = () => {
         ))}
       </Slider>
 
-      {/* Lightbox for Fullscreen Images */}
+      {/* Modal for Fullscreen Images */}
       {isOpen && (
-        <Lightbox
-          mainSrc={images[photoIndex]}
-          nextSrc={images[(photoIndex + 1) % images.length]}
-          prevSrc={images[(photoIndex + images.length - 1) % images.length]}
-          onCloseRequest={() => setIsOpen(false)}
-          onMovePrevRequest={() =>
-            setPhotoIndex((photoIndex + images.length - 1) % images.length)
-          }
-          onMoveNextRequest={() =>
-            setPhotoIndex((photoIndex + 1) % images.length)
-          }
-        >
-          <div className="share-options mt-4">
-            <FacebookShareButton url={images[photoIndex]}>
-              <button className="share-btn bg-blue-500 text-white px-4 py-2 rounded-md">Share on Facebook</button>
-            </FacebookShareButton>
-            <TwitterShareButton url={images[photoIndex]}>
-              <button className="share-btn bg-blue-400 text-white px-4 py-2 rounded-md">Share on Twitter</button>
-            </TwitterShareButton>
-            <PinterestShareButton url={images[photoIndex]}>
-              <button className="share-btn bg-red-600 text-white px-4 py-2 rounded-md">Pin it</button>
-            </PinterestShareButton>
-            <a href={images[photoIndex]} download>
-              <button className="share-btn bg-green-500 text-white px-4 py-2 rounded-md">Download Image</button>
-            </a>
+        <div className="modal fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-black bg-opacity-75 z-50">
+          <div className="relative">
+            <img
+              src={images[photoIndex]}
+              alt={`${photoIndex}`}
+              className="w-auto h-auto max-w-full max-h-screen rounded-md"
+            />
+            <button
+              className="absolute top-2 right-2 text-white bg-red-500 rounded-full p-2"
+              onClick={() => setIsOpen(false)}
+            >
+              &times; {/* Close button */}
+            </button>
+            <div className="share-options mt-4 flex justify-center space-x-2">
+              <FacebookShareButton url={images[photoIndex]}>
+                <button className="share-btn bg-blue-500 text-white px-4 py-2 rounded-md">Share on Facebook</button>
+              </FacebookShareButton>
+              <TwitterShareButton url={images[photoIndex]}>
+                <button className="share-btn bg-blue-400 text-white px-4 py-2 rounded-md">Share on Twitter</button>
+              </TwitterShareButton>
+              <PinterestShareButton url={images[photoIndex]}>
+                <button className="share-btn bg-red-600 text-white px-4 py-2 rounded-md">Pin it</button>
+              </PinterestShareButton>
+              <a href={images[photoIndex]} download>
+                <button className="share-btn bg-green-500 text-white px-4 py-2 rounded-md">Download Image</button>
+              </a>
+            </div>
           </div>
-        </Lightbox>
+        </div>
       )}
     </div>
   );
